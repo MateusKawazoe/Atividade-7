1aa81147110f411b1587f6bc1a516c5b2e8b3f6d
Mateuskwz <Matkawazoe@gmail.com>
2021-03-17 13:45:00 -0300

    Criando restante dos testes para o salário final

--- src/test/salarioFinal.test.js
+++ src/test/salarioFinal.test.js
@@ -27,8 +27,47 @@ describe('Salario Final', () => {
         expect(errors[3].message).toBe('Cargo é obrigatório!')
     })
 
-    it('Testando salário de desenvolvedor acima ou igual a 3000.00', () => {
+    it('Testando salário de desenvolvedor maior ou igual a 3000.00', () => {
         funcionario = new Funcionario('Lucas', 'leaozinho_leao@hotmail.com', 3000.00, 'DESENVOLVEDOR')
         expect([2400.00]).toBeDeepCloseTo([salarioFinal(funcionario.cargo, funcionario.salarioBase)], 2)
+    })
+
+    it('Testando salário de desenvolvedor menor que 3000.00', () => {
+        funcionario = new Funcionario('Lucas', 'leaozinho_leao@hotmail.com', 2000.00, 'DESENVOLVEDOR')
+        expect([1800.00]).toBeDeepCloseTo([salarioFinal(funcionario.cargo, funcionario.salarioBase)], 2)
+    })
+
+    it('Testando salário de DBA maior ou igual a 2000.00', () => {
+        funcionario = new Funcionario('Lucas', 'leaozinho_leao@hotmail.com', 2521.75, 'DBA')
+        expect([1891.31]).toBeDeepCloseTo([salarioFinal(funcionario.cargo, funcionario.salarioBase)], 2)
+    })
+
+    it('Testando salário de DBA menor que 2000.00', () => {
+        funcionario = new Funcionario('Lucas', 'leaozinho_leao@hotmail.com', 1533.00, 'DBA')
+        expect([1303.05]).toBeDeepCloseTo([salarioFinal(funcionario.cargo, funcionario.salarioBase)], 2)
+    })
+    it('Testando salário de testador maior ou igual a 2000.00', () => {
+        funcionario = new Funcionario('Lucas', 'leaozinho_leao@hotmail.com', 2521.75, 'TESTADOR')
+        expect([1891.31]).toBeDeepCloseTo([salarioFinal(funcionario.cargo, funcionario.salarioBase)], 2)
+    })
+
+    it('Testando salário de testador menor que 2000.00', () => {
+        funcionario = new Funcionario('Lucas', 'leaozinho_leao@hotmail.com', 1533.00, 'TESTADOR')
+        expect([1303.05]).toBeDeepCloseTo([salarioFinal(funcionario.cargo, funcionario.salarioBase)], 2)
+    })
+
+    it('Testando salário de gerente maior ou igual a 5000.00', () => {
+        funcionario = new Funcionario('Lucas', 'leaozinho_leao@hotmail.com', 7581.00, 'GERENTE')
+        expect([5306.7]).toBeDeepCloseTo([salarioFinal(funcionario.cargo, funcionario.salarioBase)], 2)
+    })
+
+    it('Testando salário de gerente menor que 5000.00', () => {
+        funcionario = new Funcionario('Lucas', 'leaozinho_leao@hotmail.com', 4911.00, 'GERENTE')
+        expect([3928.8]).toBeDeepCloseTo([salarioFinal(funcionario.cargo, funcionario.salarioBase)], 2)
+    })
+
+    test('Testando cargo inválido', () => {
+        funcionario = new Funcionario('Lucas', 'leaozinho_leao@hotmail.com', 4911.00, 'LUCAS')
+        expect('Cargo não existe!').toBe(salarioFinal(funcionario.cargo, funcionario.salarioBase))
     })
 })
