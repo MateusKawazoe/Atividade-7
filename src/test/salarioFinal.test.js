8ffa59d8b6cd1f840bbaf985b2fff4b4b2cedc6b
Mateuskwz <Matkawazoe@gmail.com>
2021-03-17 13:34:46 -0300

    Criando teste falho

--- src/test/salarioFinal.test.js
+++ src/test/salarioFinal.test.js
@@ -1,3 +1,4 @@
+const { toBeDeepCloseTo } = require('jest-matcher-deep-close-to')
 const validate = require('../validate/funcionario')
 const Funcionario = require('../model/funcionario')
 const salarioFinal = require('../main/salarioFinal')
@@ -6,7 +7,7 @@ describe('Salario Final', () => {
     let funcionario, errors
 
     test('Testando um funcionário válido', () => {
-        funcionario = new Funcionario('Lucas', 'leaozinho_leao@hotmail.com', 3000.00, 'Desenvolvedor')
+        funcionario = new Funcionario('Lucas', 'leaozinho_leao@hotmail.com', 3000.00, 'DESENVOLVEDOR')
 
         errors = validate.validate(funcionario)
         expect(errors[0]).toBe(undefined)
@@ -23,5 +24,10 @@ describe('Salario Final', () => {
         expect(errors[1].message).toBe('E-mail inválido ou em branco!')
         expect(errors[2].message).toBe('Salário base é obrigatório e só aceita números!')
         expect(errors[3].message).toBe('Cargo é obrigatório!')
+    })
+
+    it('Testando salário de desenvolvedor acima ou igual a 3000.00', () => {
+        funcionario = new Funcionario('Lucas', 'leaozinho_leao@hotmail.com', 3000.00, 'DESENVOLVEDOR')
+        expect([2400.00]).toBeDeepCloseTo([salarioFinal(funcionario.cargo, funcionario.salarioBase)], 2)
     })
 })
