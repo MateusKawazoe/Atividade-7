e958185bc0b7c1834e5858664c1af4d47f90f6f3
Mateuskwz <Matkawazoe@gmail.com>
2021-03-17 13:15:08 -0300

    Fazendo teste passar

--- src/test/salarioFinal.test.js
+++ src/test/salarioFinal.test.js
@@ -27,9 +27,9 @@ describe('Salario Final', () => {
         }
 
         errors = validate.validate(funcionario)
-        expect(errors[0]).toBe(undefined)
-        expect(errors[1]).toBe(undefined)
-        expect(errors[2]).toBe(undefined)
-        expect(errors[3]).toBe(undefined)
+        expect(errors[0].message).toBe('Nome é obrigatório e deve ter no mínimo 3 letras e no máximo 45!')
+        expect(errors[1].message).toBe('E-mail inválido ou em branco!')
+        expect(errors[2].message).toBe('Salário base é obrigatório e só aceita números!')
+        expect(errors[3].message).toBe('Cargo é obrigatório!')
     })
 })
